b72bd836d38894789a20331fef0f1361df89e8f9
Tomek Łaziuk <tlaziuk@gmail.com>
2017-03-06 10:07:40 +0100

    strings

--- README.md
+++ README.md
@@ -5,15 +5,15 @@ This is a simply bash script to install Ioncube on your Debian or Ubuntu server.
 #### Donwload & install ####
 
 Just download it and execute:
-`chmod +x eng_ioncube.sh && ./eng_ioncube.sh`
+`chmod +x ioncube.sh && ./ioncube.sh`
 
 #### Test installation ####
 
 You can run this code on a php page:
 ``` php
-    if(extension_loaded('ionCube Loader')){
-        echo "Installed";
-    } else {
-        echo "Not installed ...";
-    }
+if(extension_loaded('ionCube Loader')){
+    echo "ionCube Loader installed";
+} else {
+    echo "ionCube Loader not installed";
+}
 ```
--- ioncube.sh
+++ ioncube.sh
@@ -10,69 +10,69 @@ cd /root
 APT_GET="apt-get -y -qq"
 
 # Information
-echo -e "\\033[1;34m#### \\033[1;33m        IonCube Auto install        \\033[1;34m #### \\033[0;39m"
-echo -e "\\033[1;34m#### \\033[1;33m  By Chk : chk.harmony-hosting.com  \\033[1;34m #### \\033[0;39m"
-echo "Run script ..."
+echo -e "\\033[1;34m#### \\033[1;33m IonCube Auto install \\033[1;34m #### \\033[0;39m"
+echo -e "\\033[1;34m#### \\033[1;33m By Chk: chk.harmony-hosting.com \\033[1;34m #### \\033[0;39m"
+echo -e "Run script ..."
 
-#  Update
-echo -n "[In progress] Update package ..."
+# Update
+echo -e "[In progress] Update package ..."
 $APT_GET update
-echo -e "\r\e[0;32m[OK]\e[0m Update package                   "
+echo -e "\r\e[0;32m[OK]\e[0m Update package "
 
 
-#  32 or 64 bits
-echo -n "[In progress] Detect Linux architecture ..."
+# 32 or 64 bits
+echo -e "[In progress] Detect Linux architecture ..."
 DIST="$(command dpkg --print-architecture)"
 if [ "${DIST}" = "i386" ]; then
     DIST="x86"
 elif [ "${DIST}" = "amd64" ]; then
     DIST="x86-64"
 fi
-echo -e "\r\e[0;32m[OK]\e[0m Detect Linux architecture : $DIST              "
+echo -e "\r\e[0;32m[OK]\e[0m Detect Linux architecture: $DIST"
 
 # download tar.gz archive
-echo -n "[In progress] Donwload the tar.gz archive ..."
+echo -e "[In progress] Donwload the tar.gz archive ..."
 wget -q "http://downloads2.ioncube.com/loader_downloads/ioncube_loaders_lin_${DIST}.tar.gz"
-echo -e "\r\e[0;32m[OK]\e[0m Donwload the tar.gz archive                                  "
+echo -e "\r\e[0;32m[OK]\e[0m Donwload the tar.gz archive "
 
 # Move in directory
-echo -n "[In progress] Move to /usr/local ..."
+echo -e "[In progress] Move to /usr/local ..."
 mv ioncube_loaders_lin_${DIST}.tar.gz /usr/local/ioncube_loaders_lin_${DIST}.tar.gz
-echo -e "\r\e[0;32m[OK]\e[0m Move to /usr/local                           "
+echo -e "\r\e[0;32m[OK]\e[0m Move to /usr/local"
 
 # Go to directory
-echo -n "[In progress] Go into /usr/local ..."
+echo -e "[In progress] Go into /usr/local ..."
 cd /usr/local
-echo -e "\r\e[0;32m[OK]\e[0m Go into /usr                           "
+echo -e "\r\e[0;32m[OK]\e[0m Go into /usr"
 
 # Extract
-echo -n "[In progress] Extract tar.gz in   /usr/local ..."
+echo -e "[In progress] Extract tar.gz in /usr/local ..."
 tar -xzf ioncube_loaders_lin_${DIST}.tar.gz > /dev/null
-echo -e "\r\e[0;32m[OK]\e[0m Extract tar.gz in  /usr/local                           "
-echo -n "[In progress] remove the tar.gz file ..."
+echo -e "\r\e[0;32m[OK]\e[0m Extract tar.gz in /usr/local"
+echo "[In progress] remove the tar.gz file ..."
 rm ioncube_loaders_lin_${DIST}.tar.gz
 cd
-echo -e "\r\e[0;32m[OK]\e[0m remove the tar.gz file                        "
+echo -e "\r\e[0;32m[OK]\e[0m remove the tar.gz file "
 
-# Php version
-echo -n "[In progress] Detect PHP version ..."
+# php version
+echo -e "[In progress] Detect PHP version ..."
 VER_PHP="$(command php --version 2>'/dev/null' \
     | command head -n 1 \
     | command cut --characters=5-7)"
-echo -e "\r\e[0;32m[OK]\e[0m Detect PHP version  : $VER_PHP   "
+echo -e "\r\e[0;32m[OK]\e[0m Detect PHP version: $VER_PHP "
 
-#  Add IonCube to PHP
-echo -n "[In progress] Add IonCube to PHP ..."
+# Add IonCube to PHP
+echo -e "[In progress] Add IonCube to PHP ..."
 echo "zend_extension=/usr/local/ioncube/ioncube_loader_lin_${VER_PHP}.so" > /etc/php5/conf.d/ioncube.ini
 sed -i "1izend_extension=/usr/local/ioncube/ioncube_loader_lin_${VER_PHP}.so" /etc/php5/apache2/php.ini
-echo -e "\r\e[0;32m[OK]\e[0m Add IonCube to PHP                "
+echo -e "\r\e[0;32m[OK]\e[0m Add IonCube to PHP"
 
-#  Reboot apache2 & Php
-echo -n "[In progress] Reboot apache2 & Php ..."
+# Reboot apache2 & php
+echo -e "[In progress] Reboot apache2 & php ..."
 test -e '/etc/init.d/php5'
 command service 'php5' 'restart'
 
 test -e '/etc/init.d/apache2'
 command service 'apache2' 'force-reload'
-echo -e "\r\e[0;32m[OK]\e[0m Reboot apache2 & Php          "
-echo -e "\\033[1;32m ####  Installation réussie  #### \\033[0;39m"
+echo -e "\r\e[0;32m[OK]\e[0m Reboot apache2 & php"
+echo -e "\\033[1;32m #### Installation réussie #### \\033[0;39m"
